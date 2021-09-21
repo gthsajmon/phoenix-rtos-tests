6f52712e90dbc6aa7e76e4dc9ef48794aa43dbe4
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2021-09-21 11:56:25 +0200

    busybox: temporarily disable check if posixsrv has been run properly, because of waitpid WNOHANG problem
    JIRA: RTOS-59

--- busybox/test_busybox.c
+++ busybox/test_busybox.c
@@ -10,42 +10,43 @@ int main(int argc, char **argv)
 {
 	int ret;
 	DIR *dir = opendir("/bin/testsuite");
+	char *const arg[] = { "/bin/posixsrv", NULL };
 
 	if (dir) {
 		closedir(dir);
 	}
 	else if (ENOENT == errno) {
 		fprintf(stderr, "There is no busybox test suite to run, build project with \"long_test\"\n");
-		return (1);
+		return 1;
 	}
 	else {
 		fprintf(stderr, "There is problem with opening existing /bin/testsuite directory: %s\n", strerror(errno));
-		return (1);
+		return 1;
 	}
 	if ((ret = vfork()) < 0) {
 		fprintf(stderr, "vfork function failed: %s\n", strerror(errno));
-		return (1);
+		return 1;
 	}
 	else if (!ret) {
-		execv("/bin/posixsrv", argv);
+		execv("/bin/posixsrv", arg);
 		fprintf(stderr, "exec function failed: %s\n", strerror(errno));
 		_exit(EXIT_FAILURE);
 	}
 	/* without this delay sometimes posixsrv is not yet running before the next function call */
-	usleep(100000);
-	if (waitpid(ret, NULL, WNOHANG) > 0) {
-		fprintf(stderr, "Posixsrv failed to run in background\n");
+	usleep(500000);
+
+	/* check if posixsrv has been ran properly temporarily disabled, because of problems with waitpid WNOHANG */
+	/* if (waitpid(ret, NULL, 0) > 0) {
+	/ 	fprintf(stderr, "Posixsrv failed to run in background\n");
+	/	return 1;
+	/ } */
+
+	if ((ret = system("cd /bin/testsuite/ && export PATH=/bin:/sbin:/usr/bin:/usr/sbin && echo rrrr && ./runtest -v")) < 0) {
+		fprintf(stderr, "system function failed: %s\n", strerror(errno));
 		return 1;
 	}
-	else
-		printf("Posixsrv ran in background\n");
 
-	if ((ret = system("cd /bin/testsuite/ && export PATH=/bin:/sbin:/usr/bin:/usr/sbin && ./runtest -v")) < 0) {
-		fprintf(stderr, "system function failed: %s\n", strerror(errno));
-		return (1);
-	}
-
-	printf("\nRuntest done\n\n");
+	printf("\n****The Busybox Test Suite completed****\n\n");
 
 	return 0;
 }
